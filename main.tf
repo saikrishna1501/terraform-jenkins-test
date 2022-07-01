7ed87ec4b0d3a0f9955e549f73de30441165f0fe
saikrishna1501 <saikrishna1501@gmail.com>
2022-07-01 19:02:35 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "sg" {
+  name        = "sgtest"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-094feaf7ee33d484b"
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg-test"
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-00ad5a974f5e8af44"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = "public-ec2"
+  }
+}
+
